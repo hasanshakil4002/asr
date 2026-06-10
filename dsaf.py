84ee3eea350d3fac2a4d93cab74a86df094bf5dd
md-shakil24 <mohammedshakilh04@gmail.com>
2026-06-10 16:56:42 +0600

    dsaf.py file logic created

---
+++ dsaf.py
@@ -0,0 +1,6 @@
+f=int(input("Enter a number: "))
+g=int(input("Enter another number: "))
+sum=f+g
+print("The sum of", f, "and", g, "is:", sum)    
+print("This is a simple Python program to add two numbers.")
+print("Thank you for using this program!")
